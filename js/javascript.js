89fa79d7f2dd972b2f2bf46e9cd70a24ea03aaa0
SJ <SJ@sj.com>
2021-08-04 18:30:53 +0200

    initialisation du projet

---
+++ js/javascript.js
@@ -0,0 +1,15 @@
+//Génère la pyramide
+function generate(){
+	//Initialisation des variables et de l'affichage (suppression des éventuels message d'erreur et pyramide)
+	
+	//Si la valeur entrée par l'utilisateur n'est pas valide, affichage du message d'erreur
+	
+	//Sinon, construction de la pyramide
+		
+	//Vider le champ d'entrée (largeur)
+}
+
+//Supprime la pyramide
+function reset(){
+	
+}
