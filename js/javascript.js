2a7c25cea3361eb225f3c5b849b8bd15bb557aed
sebjossi <sebjossi@gmail.com>
2021-08-05 14:26:18 +0200

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -24,6 +24,6 @@ function generate(){
 
 //Supprime la pyramide
 function reset(){
-	document.getElementById("output").innerHTML = "";
+	document.getElementById("output").innerHTML = "empty";
 	document.getElementById("message").innerHTML = "";
 }
