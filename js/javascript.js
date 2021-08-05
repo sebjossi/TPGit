ff63c4f8188b9ec4c983615fa2e95ccc5a4f53dd
sebjossi <sebjossi@gmail.com>
2021-08-05 11:11:47 +0200

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,25 @@
 //Génère la pyramide
 function generate(){
-	//Initialisation des variables et de l'affichage (suppression des éventuels message d'erreur et pyramide)
-	
-	//Si la valeur entrée par l'utilisateur n'est pas valide, affichage du message d'erreur
-	
-	//Sinon, construction de la pyramide
-		
-	//Vider le champ d'entrée (largeur)
+	let messageDiv= document.getElementById("message");
+	let width = document.getElementById("PyramideWidth").value;
+	let outputDiv = document.getElementById("output");
+	messageDiv.innerHTML = "";
+	outputDiv.innerHTML = "";
+	let result = "";
+	if(width<1 || width>20 || isNaN(width)){
+		messageDiv.innerHTML = "Entrée non valide!";
+	}
+	else{
+		for(i=1;i<=width;i++){
+			result += "<div class='pyramideRow'>";
+			for(j=0;j<i;j++){
+				result += "<img src='img/brick.jpg' width='50' height='50'>";
+			}
+			result += "</div>";
+		}
+		outputDiv.innerHTML = result;
+	}
+	document.getElementById("PyramideWidth").value = "";
 }
 
 //Supprime la pyramide
